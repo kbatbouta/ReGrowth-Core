d9b1f5f3bd36b041984a7238f8c6c938c19ea2c0
Taranchuk <nfhfyxer@gmail.com>
2020-10-22 14:19:36 +0300

    update

--- 1.2/Source/ReGrowthCore/WeatherOverlay_DrizzleRain.cs
+++ 1.2/Source/ReGrowthCore/WeatherOverlay_DrizzleRain.cs
@@ -16,14 +16,16 @@ namespace ReGrowthCore
 			worldPanDir2.Normalize();
 		}
 
-        public override void TickOverlay(Map map)
-        {
+		public override void TickOverlay(Map map)
+		{
 			if (worldOverlayMat == null)
-            {
+			{
 				worldOverlayMat = MaterialPool.MatFrom("Weather/DrizzleRainWorldOverlay");
 				worldOverlayMat.CopyPropertiesFromMaterial(MatLoader.LoadMat("Weather/RainOverlayWorld"));
 				worldOverlayMat.shader = MatLoader.LoadMat("Weather/RainOverlayWorld").shader;
-				worldOverlayMat.mainTexture = ContentFinder<Texture2D>.Get("Weather/DrizzleRainWorldOverlay");
+				worldOverlayMat.SetTexture("_MainTex", ContentFinder<Texture2D>.Get("Weather/DrizzleRainWorldOverlay"));
+				worldOverlayMat.SetTexture("_MainTex2", ContentFinder<Texture2D>.Get("Weather/DrizzleRainWorldOverlay"));
+				//this.worldOverlayMat.SetColor("_TuningColor", new Color(1, 1, 1, 1)); //0.2720588f, 0.2720588f, 0.2720588f, 0.05882353f));
 			}
 			base.TickOverlay(map);
 		}
