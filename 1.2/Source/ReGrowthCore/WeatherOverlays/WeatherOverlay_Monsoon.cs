a94fdc5599620597cf732f64c6c0e410cbf0afb2
Taranchuk <nfhfyxer@gmail.com>
2020-10-25 09:52:16 +0200

    update

---
+++ 1.2/Source/ReGrowthCore/WeatherOverlays/WeatherOverlay_Monsoon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ReGrowthCore
+{
+	public class WeatherOverlay_Monsoon : SkyOverlay
+	{
+		public WeatherOverlay_Monsoon()
+		{
+			worldOverlayMat = null;
+			worldOverlayPanSpeed1 = 0.015f;
+			worldPanDir1 = new Vector2(-0.25f, -1f);
+			worldPanDir1.Normalize();
+			worldOverlayPanSpeed2 = 0.022f;
+			worldPanDir2 = new Vector2(-0.24f, -1f);
+			worldPanDir2.Normalize();
+		}
+
+		public override void TickOverlay(Map map)
+		{
+			if (worldOverlayMat == null)
+			{
+				worldOverlayMat = MaterialPool.MatFrom("Weather/MonsoonWorldOverlay");
+				worldOverlayMat.CopyPropertiesFromMaterial(MatLoader.LoadMat("Weather/RainOverlayWorld"));
+				worldOverlayMat.shader = MatLoader.LoadMat("Weather/RainOverlayWorld").shader;
+				worldOverlayMat.SetTexture("_MainTex", ContentFinder<Texture2D>.Get("Weather/MonsoonWorldOverlay"));
+				worldOverlayMat.SetTexture("_MainTex2", ContentFinder<Texture2D>.Get("Weather/MonsoonWorldOverlay"));
+				//this.worldOverlayMat.SetColor("_TuningColor", new Color(1, 1, 1, 1)); //0.2720588f, 0.2720588f, 0.2720588f, 0.05882353f));
+			}
+			base.TickOverlay(map);
+		}
+	}
+}
---
+++ 1.2/Source/ReGrowthCore/WeatherOverlays/WeatherOverlay_Sandstorm.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ReGrowthCore
+{
+	public class WeatherOverlay_Sandstorm : SkyOverlay
+	{
+		public WeatherOverlay_Sandstorm()
+		{
+			worldOverlayMat = null;
+			worldOverlayPanSpeed1 = 0.0005f;
+			worldOverlayPanSpeed2 = 0.0004f;
+			worldPanDir1 = new Vector2(1f, 1f);
+			worldPanDir2 = new Vector2(1f, -1f);
+		}
+
+		public override void TickOverlay(Map map)
+		{
+			if (worldOverlayMat == null)
+			{
+				worldOverlayMat = MaterialPool.MatFrom("Weather/SandstormWorldOverlay");
+				worldOverlayMat.CopyPropertiesFromMaterial(MatLoader.LoadMat("Weather/FogOverlayWorld"));
+				worldOverlayMat.shader = MatLoader.LoadMat("Weather/FogOverlayWorld").shader;
+				worldOverlayMat.SetTexture("_MainTex", ContentFinder<Texture2D>.Get("Weather/SandstormWorldOverlay"));
+				worldOverlayMat.SetTexture("_MainTex2", ContentFinder<Texture2D>.Get("Weather/SandstormWorldOverlay"));
+				//this.worldOverlayMat.SetColor("_TuningColor", new Color(1, 1, 1, 1)); //0.2720588f, 0.2720588f, 0.2720588f, 0.05882353f));
+			}
+			base.TickOverlay(map);
+		}
+	}
+}
